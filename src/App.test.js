282758f5aed39286ffb9ce3f8f9fc60d51486ce0
Muaz Zelinkich <muazzelinkic@gmail.com>
2023-07-28 02:58:03 +0300

    feat(App.test): add test component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Profile from './component/Profile';
+import Users from './component/Users';
+import { createRenderer } from 'react-dom/test-utils';
+import InputContainer from './component/InputContainer';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("profile", () => {
+  const profile = createRenderer(<Profile />).toJSON();
+  expect(profile).toMatchSnapshot();
+});
+
+test("fetch users", () => {
+  const users = createRenderer(<Users />).toJSON();
+  expect(users).toMatchSnapshot();
+});
+
+test("ipunt", () => {
+  const input = createRenderer(<InputContainer />).toJSON();
+  expect(input).toMatchSnapshot();
+});
+
+
+
